7304557ebb556d792c33b2df9b9c6252fd0562cb
Shashank <shashank.1@tripjack.com>
2026-03-18 11:43:07 +0530

    adding Google Analytics

--- src/components/analytics/GAListener.tsx
+++ src/components/analytics/GAListener.tsx
@@ -1,22 +1,18 @@
 "use client";
 
 import { useEffect } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 
 export default function GAListener() {
     const pathname = usePathname();
-    const searchParams = useSearchParams();
 
     useEffect(() => {
         if (!window.gtag) return;
 
-        const url =
-            pathname + (searchParams.toString() ? `?${searchParams}` : "");
-
         window.gtag("config", "G-ETDQMP0BYH", {
-            page_path: url,
+            page_path: pathname,
         });
-    }, [pathname, searchParams]);
+    }, [pathname]);
 
     return null;
 }
